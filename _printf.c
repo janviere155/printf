8d4f625f4a0cffe68b4cb366c185826752e1a655
janviere155 <janviere155@gmail.com>
2023-06-22 23:06:05 +0300

    'printf'

--- _printf.c
+++ _printf.c
@@ -1,49 +1,45 @@
 #include "main.h"
-
-#include <stdarg.h>
-
 /**
- * _printf - produces output according to format
- * @format: format string containing zero or more directives
- * Return: the number of characters printed, excluding null byte
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
  */
-
-int _printf(const char *format, ...)
-
+int _printf(const char * const format, ...)
 {
-	data_t types[] = {
-	{"c", _printchar}, {"s", _printstring}, {"i", _printint},
-	{"R", _rot13}, {"d", _printint}, {"r", _printrev}};
+	convert_match m[] = {
+		{"%s", printf_string}, {"%c", printf_char},
+		{"%%", printf_37},
+		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
+		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+	};
 
-	int i, j, counter = 0;
-	va_list ap;
-	va_start(ap, format);
+	va_list args;
+	int i = 0, j, len = 0;
+
+	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	for (i = 0; format[i] != '\0'; i++)
+
+Here:
+	while (format[i] != '\0')
+	{
+		j = 13;
+		while (j >= 0)
 		{
-			if (format[i] != '%')
-				counter += _putchar(format[i]);
-			else if (format[i + 1] == '%')
-				{
-					counter += _putchar('%');
-					i++;
-				}
-													else if (format[i + 1] != 'c' && format[i + 1] != 's'
-				 && format[i + 1] != 'i' && format[i + 1] != 'd'
-	 			 && format[i + 1] != 'R' && format[i + 1] != 'r')
-				{
-															counter += _putchar('%');
-					counter += _putchar(format[i + 1]);
-					i++;
-				}
-													else
-				{
-															for (j = 0; j < 6; j++)
-						if (format[i + 1] == *types[j].type)
-					counter += types[j].func(ap);																										i++;
-																									}
-															}
-				va_end(ap);
-				return (counter);
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		len++;
+		i++;
+	}
+	va_end(args);
+	return (len);
 }
